3882485796112539824f99202cbbf2a01f6a8f9c
linhora <paulcordier22@gmail.com>
2015-11-23 09:07:39 +0100

    Copie des fichiers TP03

--- workspace/iuttp/src/edu/iut/app/ApplicationSession.java
+++ workspace/iuttp/src/edu/iut/app/ApplicationSession.java
@@ -7,26 +7,35 @@ import java.util.logging.Logger;
 
 public class ApplicationSession {
 	
-	// Exercice 1 : Gérer l'internationation
 	protected ResourceBundle resourceBundle;
 	protected Locale locale;
-	
-	// Exercice 2 : Logger
 	protected Logger sessionGuiLogger;
 	protected Logger sessionExceptionLogger;
+	protected String[] months;
+	protected String[] days;
 
 
 	private static ApplicationSession session = null;
 	private ApplicationSession() {
-		/* Definir US comme locale par défaut */
-		Locale.setDefault(Locale.US);
-		
 		locale = Locale.getDefault();
-		resourceBundle = ResourceBundle.getBundle("res.properties", locale);
-		sessionGuiLogger = Logger.getLogger(Logger.GLOBAL_LOGGER_NAME);
+		resourceBundle = ResourceBundle.getBundle("edu.iut.resources.strings.res");
+		sessionGuiLogger = Logger.getLogger("IUTTrain");
 		sessionGuiLogger.setLevel(Level.ALL);
-		sessionExceptionLogger = Logger.getLogger(Logger.GLOBAL_LOGGER_NAME);
+		sessionExceptionLogger = Logger.getLogger("IUTException");
 		sessionExceptionLogger.setLevel(Level.ALL);
+		
+		days = new String[7];
+		days[0] = getString("monday"); days[1] =  getString("tuesday"); days[2] =  getString("wednesday");
+		days[3] = getString("thursday");   days[4] =  getString("friday"); days[5] =  getString("saturday");
+		days[6] = getString("sunday"); 
+		
+		months = new String[12];
+		months[0] = getString("january"); months[1] =  getString("february"); months[2] =  getString("march");
+		months[3] = getString("april");   months[4] =  getString("may");      months[5] =  getString("june");
+		months[6] = getString("july"); months[7] =  getString("august"); months[8] =  getString("september");
+		months[9] = getString("october"); months[10] =  getString("november"); months[11] =  getString("december");
+		
+		
 	}
 	
 	
@@ -47,11 +56,18 @@ public class ApplicationSession {
 	public void setLocale(Locale locale){
 		this.locale = locale;
 		Locale.setDefault(this.locale);
-		resourceBundle= ResourceBundle.getBundle(baseName, locale)
+		resourceBundle=ResourceBundle.getBundle("edu.iut.resources.strings.res");
 	}
 	
 	public String getString(String key) {
 		return resourceBundle.getString(key);
+	}
+	
+	public String[] getDays() {
+		return days;
+	}
+	public String[] getMonths() {
+		return months;
 	}
 	
 	
